bc4cdbc8b3029d471ab23906181bfa780a5aac6d
JonathanUSG <23030011@itesa.edu.mx>
2024-10-30 07:20:27 -0600

    Version_01: Se creó la arquitectura del proyecto.

---
+++ ListaLigada.UI.Consola/Program.cs
@@ -0,0 +1,15 @@
+﻿using ListaLigada.Logica;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Lista Ligada Simple\n");
+        var singleList =new SingleList<string>();
+        singleList.Add("Juan");
+        singleList.Add("José");
+        singleList.Add("Jan");
+
+        Console.WriteLine(singleList);
+    }
+}
